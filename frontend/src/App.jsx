5901c2a95891cd5479894859c28538990a7f86cf
erickkipkoech <cheruiyotkipkoech612@gmail.com>
2023-05-02 23:39:33 +0300

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,8 @@
-import { Link, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
-import Login from "./components/login";
-import Dashboard from "./components/dashboard";
-import Register from "./components/register";
 
 function App() {
   return (
     <div>
-  
-      <Routes>
-        <Route path="/" element={<><Navbar /><Dashboard /></>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Login />} />
-      </Routes>
+App
     </div>
   );
 }
